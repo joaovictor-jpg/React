482cc37851df2a91c8947faf667f958c018a1c3e
joao victor <joaovictormdasilva676@gmail.com>
2023-06-08 19:59:18 -0300

    loader component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,6 +2,7 @@ import Message from '../layout/Message';
 import Container from '../layout/Container';
 import LinkButton from '../layout/LinkButton';
 import ProjectCard from '../project/ProjectCard';
+import Loading from '../layout/Loading';
 
 import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
@@ -11,6 +12,7 @@ import style from './projects.module.css';
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
 
@@ -21,14 +23,20 @@ function Projects() {
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/project', {
+        setTimeout(() => {
+            fetch('http://localhost:5000/project', {
             mathod: 'GET',
             headers: {
                 'Content-type': 'application/json',
             },
-        }).then((resp) => resp.json()).then((data) => {
-                setProjects(data)
-            }).catch((err) => console.log(err))
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProjects(data)
+            setRemoveLoading(true)
+        })
+        .catch((err) => console.log(err))
+        }, 300);
     }, [])
 
     return ( 
@@ -47,6 +55,10 @@ function Projects() {
                         category={project.category.name}
                         Key={project.id}
                     />)}
+                    {!removeLoading && <Loading />}
+                    {removeLoading && projects.lengtg === 0 && (
+                        <p>Não há projetos cadastrados!</p>
+                    )}
             </Container>
         </div>
      );
